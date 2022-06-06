6b804382581e28350cd07d0f9fd59292dd2a455e
Victor Carli <victorcarlibr@gmail.com>
2022-06-06 10:33:45 -0300

    pagination mirage

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,4 @@
-import { createServer, Model, Factory } from 'miragejs'
+import { createServer, Model, Factory, Response } from 'miragejs'
 import { faker } from '@faker-js/faker'
 import { User } from '../../types/users'
 
@@ -30,7 +30,18 @@ export function makeServer() {
 			this.namespace = 'api'
 			this.timing = 750
 
-			this.get('/users')
+			this.get('/users', function(schema, request) {
+				const { page = 1, per_page = 10 } = request.queryParams
+
+				const total = schema.all('user').length
+				const pageStart = (Number(page) - 1) * Number(per_page)
+				const pageEnd = pageStart + Number(per_page)
+
+				const users = this.serialize(schema.all('user')).users.slice(pageStart, pageEnd)
+
+				return new Response(200, { 'x-total-count': String(total) }, { users })
+			})
+
 			this.post('/users')
 
 			this.namespace = ''
